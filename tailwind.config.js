cd0f091f4d39323d297cf3fc1c1d3c8350c22444
Shreenidhi Sashisekar <93395263+shree1767@users.noreply.github.com>
2022-10-03 00:16:32 +0530

    Navbar and Landing fixes ✨

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,28 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation:{
+        'slow-spin': 'spin 10s linear infinite',
+      },
+
+    },
+    filter: { // defaults to {}
+      'none': 'none',
+      'grayscale': 'grayscale(1)',
+      'invert': 'invert(1)',
+      'sepia': 'sepia(1)',
+    },
+    backdropFilter: { // defaults to {}
+      'none': 'none',
+      'blur': 'blur(20px)',
+    },
+    variants: {
+      filter: ['responsive'], // defaults to ['responsive']
+      backdropFilter: ['responsive'], // defaults to ['responsive']
+    },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-filters'),
+  ],
 }
