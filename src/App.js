479a0717a943f45b011d2d3ff5313bc49912b43a
Shreenidhi Sashisekar <93395263+shree1767@users.noreply.github.com>
2022-10-02 11:16:32 +0530

    Navbar and Landing fixes ✨

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
+import Landing from './Components/Landing/Landing';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Landing/>
       <Footer />
     </div>
   );
