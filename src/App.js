da993aaedb6b9fa5c573b8195c6e819d51fe8df6
NotPraneeth <rsphaha@gmail.com>
2022-10-05 19:38:20 +0530

    a very basic prototype of the first about page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 import Landing from './Components/Landing/Landing';
+import About from './Components/About/About'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Landing/>
+      <About/>
       <Footer />
     </div>
   );
