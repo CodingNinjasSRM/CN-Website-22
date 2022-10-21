004459ffe27ab05b3df708b02b73362ec12dd40e
Shreenidhi Sashisekar <93395263+shree1767@users.noreply.github.com>
2022-10-22 00:03:49 +0530

    team page created

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 import Landing from './Components/Landing/Landing';
+import Technical from './Components/Technical/Technical';
+import Team from './Components/Team/Team';
 
 function App() {
   return (
+    
     <div className="App">
+      
       <Navbar />
       <Landing/>
+      {/* <Team/> */}
       <Footer />
+      
     </div>
+   
   );
 }
 
