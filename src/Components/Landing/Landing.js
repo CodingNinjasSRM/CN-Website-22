292d677221c7458544ce9335fd5318864f3856a6
Shreenidhi Sashisekar <93395263+shree1767@users.noreply.github.com>
2022-10-30 23:47:41 +0530

    design changes

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import Aos from 'aos'
+import "aos/dist/aos.css";
 import style from './Landing.module.css'
 import ninja from './assets/ninja.svg'
 import group from './assets/Group.svg'
@@ -18,7 +20,13 @@ import "react-multi-carousel/lib/styles.css";
 import { Link } from 'react-router-dom'
 
 
+
 function Landing() {
+
+    useEffect(() =>{
+        Aos.init({duration:1500});
+    }, []);
+
     const responsive = {
         superLargeDesktop: {
           // the naming can be any, depends on you.
@@ -41,16 +49,18 @@ function Landing() {
 
     return (
         <>
-        <div>
-            <div className="mx-auto left-6 max-w-screen-lg overflow-hidden justify-center container pb-10 ">
-                <img src={ninja} className="inset-0 object-cover mx-auto mt-10"  alt="Ninja"/>
-                <img src={group} className="md:w-auto w-2/3 animate-spin animate-slow-spin absolute inset-0 object-cover mx-auto mt-20 top-7 md:mt-20 md:bottom-2 md:top-16 "  alt="Group"/>    
+        <main className="bg">
+        <div className='mt-10 md:pb-10 pb-20'>
+            <div className="mx-auto  left-6 max-w-screen-lg overflow-hidden justify-center container pb-20 ">
+                <img src={ninja} className="inset-0 object-cover mx-auto mt-8"  alt="Ninja"/>
+                <img src={group} className="md:w-auto w-2/3 animate-spin animate-slow-spin absolute inset-0 object-cover mx-auto mt-20 top-20 md:mt-20 md:bottom-2 md:top-22 "  alt="Group"/>    
             </div>
         </div>
-
+        </main>
+        
         {/* About */}
-        <div className='bg-stone-800 bg-opacity-40'>
-            <div className='md:text-7xl text-5xl text-transparent bg-clip-text bg-gradient-to-r from-orange-700 to-black md:text-left text-center md:ml-14 ml-5 font-extrabold md:mb-10 mb-5 md:pb-12 pb-2 md:pt-20 pt-10 mt-10'>ABOUT US</div>
+        <div className='bg-zinc-900 bg-opacity-40'>
+            <div data-aos="fade-up" className='md:text-7xl text-5xl text-transparent bg-clip-text bg-gradient-to-r from-orange-700 to-black md:text-left text-center md:ml-14 ml-5 font-extrabold md:mb-10 mb-5 md:pb-12 pb-2 md:pt-20 pt-10 '>ABOUT US</div>
             <div className='grid grid-cols-1 gap-5 py-8 px-3 md:grid-cols-2 pb-20'>
                 <div className='text-white md:text-left text-center mx-2 md:pl-5 md:ml-7 pl-3 pr-3 md:text-2xl text-1 md:pb-20 mb-10'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum et ante ac fringilla. Pellentesque et felis metus. Cras facilisis ullamcorper magna .
@@ -64,12 +74,13 @@ function Landing() {
         </div>
 
         {/* Domains */}
+    <main className="bg">
         <div>
-            <div className='md:text-7xl text-5xl text-transparent bg-clip-text bg-gradient-to-r from-orange-700 to-black md:text-left text-center md:ml-14 ml-5 font-extrabold md:mb-10 mb-5 md:pb-12 pb-2 md:pt-20 pt-10 mt-10'>DOMAINS</div>
+            <div  data-aos="fade-up" className='md:text-7xl text-5xl text-transparent bg-clip-text bg-gradient-to-r from-orange-700 to-black md:text-left text-center md:ml-14 ml-5 font-extrabold md:mb-10 mb-5 md:pb-12 pb-2 md:pt-20 pt-10 mt-10'>DOMAINS</div>
         </div>
         <div className='grid grid-cols-1 gap-5 py-8 px-3 md:grid-cols-2 mt-20 mb-20'>
             {/* Technical */}
-            <div>
+            <div data-aos="fade-up">
                 
                 <div className='relative'>
                     <h3 className='text-orange-700 text-4xl font-light text-left ml-11 pt-20 pb-5'>Technical</h3>
@@ -85,13 +96,13 @@ function Landing() {
                 
             </div>
             {/* image */}
-            <div className='hidden md:block xl:ml-20 xl:pl-14 ml-10 pr-5'>
+            <div  data-aos="zoom-in" className='hidden md:block xl:ml-20 xl:pl-14 ml-10 pr-5'>
                 <img className="lg:pl-10 pl-0" src={img}/>
             </div>
         </div>
-        <div className='grid grid-cols-1 gap-5 py-8 px-3 md:grid-cols-2 mt-20 mb-20'>
+        <div data-aos="fade-up" className='grid grid-cols-1 gap-5 py-8 px-3 md:grid-cols-2 mt-20 mb-20'>
             {/* image */}
-            <div className='hidden md:block xl:ml-1 xl:pl-1 ml-10 pr-5'>
+            <div  data-aos="zoom-in" className='hidden md:block xl:ml-1 xl:pl-1 ml-10 pr-5'>
                 <img className="lg:pl-10 pl-0" src={img}/>
             </div>
              {/* Corporates */}
@@ -115,7 +126,7 @@ function Landing() {
         </div>
         <div className='grid grid-cols-1 gap-5 py-8 px-3 md:grid-cols-2 mt-20 mb-20'>
              {/* Creatives*/}
-             <div>
+             <div data-aos="fade-up">
                 
                 <div className='relative'>
                     <h3 className='text-orange-700 text-4xl font-light text-left ml-11 pt-20 pb-5'>Creatives</h3>
@@ -131,20 +142,22 @@ function Landing() {
                 
             </div>
             {/* image */}
-            <div className='hidden md:block xl:ml-20 xl:pl-14 ml-10 pr-5'>
+            <div  data-aos="zoom-in" className='hidden md:block xl:ml-20 xl:pl-14 ml-10 pr-5'>
                 <img className="lg:pl-10 pl-0" src={img}/>
             </div>
         </div>
-
+    </main>
         {/* Events */}
+        
         <div>
-            <div className='md:text-7xl text-5xl text-transparent bg-clip-text bg-gradient-to-r from-orange-700 to-black text-center font-extrabold md:mb-10 mb-5 pb-2 md:pt-20 pt-10 mt-10'>EVENTS</div>
+            <div data-aos="fade-up" className='md:text-7xl text-5xl text-transparent bg-clip-text bg-gradient-to-r from-orange-700 to-black text-center font-extrabold md:mb-10 mb-5 pb-2 md:pt-20 pt-10 mt-10'>EVENTS</div>
         </div>
         <div className='pb-10'>
             <div className='text-white text-center md:text-xl'>Know more about the upcoming <Link to='/'><span className='text-orange-700'>Events</span></Link></div>
         </div>
+       
         <Carousel responsive={responsive} 
-        className='md:flex bg-neutral-800 bg-opacity-40 pt-20 justify-center pb-20'
+        className='md:flex bg-zinc-900  bg-opacity-40 pt-20 justify-center pb-20'
         additionalTransfrom={0}
         arrows={false}
         autoPlay
@@ -200,7 +213,7 @@ function Landing() {
             }} src={shuriken} className='md:w-50 md:h-50 '/>
        
         </Carousel>
-        
+       
 
         <div className='justify-center md:mt-10 pt-20 pb-20 md:mb-10 px-20 '>
             <img src={ninja2} className="mx-auto"/>
