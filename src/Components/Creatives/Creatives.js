822bc8a1673098c543a5908a0b1d604e340e9cb1
Shreenidhi Sashisekar <93395263+shree1767@users.noreply.github.com>
2022-11-06 23:22:09 +0530

    design changes

--- src/Components/Creatives/Creatives.js
+++ src/Components/Creatives/Creatives.js
@@ -9,21 +9,21 @@ import { Link } from 'react-router-dom'
 function Creatives() {
     return (
         <div className={`${style.creativebg}`}>
-        <div className={`md:text-7xl text-5xl text-transparent bg-clip-text font-extrabold md:pt-20 md:mt-12 mt-12 pt-10 ${style.creativeheading}`}>CREATIVES</div>
-        <div className={`md:text-9xl  text-6xl text-transparent bg-clip-text font-extrabold md:mb-10 mb-5 pb-2 ${style.domainsheading}`}>DOMAIN</div>
+        <div className={`md:text-[84px] text-[35px] text-transparent bg-clip-text font-[800] md:pt-20 md:mt-10 mt-6 pt-20 ${style.creativeheading}`}>CREATIVES</div>
+        <div className={`md:text-[130px] text-[50px] text-transparent bg-clip-text font-[800] md:mb-10 pb-2 ${style.domainsheading}`}>DOMAIN</div>
         <div className='md:flex md:pb-10 justify-center'>
-            <p className='text-left md:text-xl mx-auto font-light text-white px-16 pt-10 md:px-20 md:pt-10 md:mx-2 md:pb-0 pb-20'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum et ante ac fringilla. Pellentesque et felis metus. Cras facilisis ullamcorper magna.
-            orem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum et ante ac fringilla. Pellentesque et felis metus. Cras facilisis ullamcorper magna .<p className='md:pt-10 pt-10 mt-5 md:mt-5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum et ante ac fringilla. Pellentesque et felis metus. Cras facilisis ullamcorper magna.
+            <p className='md:text-left md:text-xl text-[15px] mx-auto font-light text-white px-16 pt-10 md:px-20 md:pt-10 md:mx-2 md:pb-0 pb-20'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum et ante ac fringilla. Pellentesque et felis metus. Cras facilisis ullamcorper magna.
+            orem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum et ante ac fringilla. Pellentesque et felis metus. Cras facilisis ullamcorper magna .<p className='md:block hidden md:pt-10 pt-10 mt-5 md:mt-5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum et ante ac fringilla. Pellentesque et felis metus. Cras facilisis ullamcorper magna.
             orem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum et ante ac fringilla. Pellentesque et felis metus. Cras facilisis ullamcorper magna .</p>
             </p>
             <img className='md:mb-5 md:block hidden md:w-100 md:h-100 w-1/2 h-1/2 md:mr-10 md:pr-20' src={img}/>
         </div>
         <div className='flex justify-center pb-20'>
-                <Link to='/corporate'><img src={leftar} className='mx-10'/></Link>
-                <img src={middle}/>
-                <Link to='/'><img src={rightar} className='mx-10'/></Link>
-            </div>
+            <Link to='/corporate'><img src={leftar} className='mx-10 md:mx-10'/></Link>
+            <img src={middle} className='hidden md:block'/>
+            <Link to='/'><img src={rightar} className='mx-10 md:mx-10'/></Link>
         </div>
+    </div>
     )
 }
 
