b103fbd5d48985fc391bfb1316d143bbb61a3f9a
Shreenidhi Sashisekar <93395263+shree1767@users.noreply.github.com>
2022-11-26 21:15:01 +0530

    domains changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Chatbot from "./Components/Chatbot/Chatbot";
-import Corporate from "./Components/Corporate/Corporate";
-import Creatives from "./Components/Creatives/Creatives";
 import Footer from "./Components/Footer/Footer";
 import Landing from "./Components/Landing/Landing";
 import Navbar from "./Components/Navbar/Navbar";
 import Team from "./Components/Team/Team";
 import Technical from "./Components/Technical/Technical";
+import Nontechnical from "./Components/Nontechnical/Nontechnical";
 
 function App() {
   return (
@@ -16,9 +15,8 @@ function App() {
       <Chatbot />
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="technical" element={<Technical />} />
-        <Route path="corporate" element={<Corporate />} />
-        <Route path="creative" element={<Creatives />} />
+        <Route path="technical" element={<Technical/>} />
+        <Route path="nontechnical" element={<Nontechnical/>} />
         <Route path="team" element={<Team />} />
       </Routes>
       <Footer />
--- src/Components/Nontechnical/Nontechnical.js
+++ src/Components/Nontechnical/Nontechnical.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import style from './Creatives.module.css'
+import style from './Nontechnical.module.css'
 import img from './assets/Img1.svg'
 import leftar from './assets/leftar.svg'
 import middle from './assets/middle.svg'
 import rightar from './assets/rightar.svg'
 import { Link } from 'react-router-dom'
 
-function Creatives() {
+function Nontechnical() {
     return (
-        <div className={`${style.creativebg}`}>
-        <div className={`md:text-[84px] text-[35px] text-transparent bg-clip-text font-[800] md:pt-20 md:mt-10 mt-6 pt-20 ${style.creativeheading}`}>CREATIVES</div>
+        <div className={`${style.corporatebg}`}>
+        <div className={`md:text-[84px] text-[35px] text-transparent bg-clip-text font-[800] md:pt-20 md:mt-10 mt-6 pt-20 ${style.corporateheading}`}>NON TECHNICAL</div>
         <div className={`md:text-[130px] text-[50px] text-transparent bg-clip-text font-[800] md:mb-10 pb-2 ${style.domainsheading}`}>DOMAIN</div>
         <div className='md:flex md:pb-10 justify-center'>
             <p className='md:text-left md:text-xl text-[15px] mx-auto font-light text-white px-16 pt-10 md:px-20 md:pt-10 md:mx-2 md:pb-0 pb-20'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum et ante ac fringilla. Pellentesque et felis metus. Cras facilisis ullamcorper magna.
@@ -19,12 +19,12 @@ function Creatives() {
             <img className='md:mb-5 md:block hidden md:w-100 md:h-100 w-1/2 h-1/2 md:mr-10 md:pr-20' src={img}/>
         </div>
         <div className='flex justify-center pb-20'>
-            <Link to='/corporate'><img src={leftar} className='mx-10 md:mx-10'/></Link>
+            <Link to='/technical'><img src={leftar} className='mx-10 md:mx-10'/></Link>
             <img src={middle} className='hidden md:block'/>
-            <Link to='/'><img src={rightar} className='mx-10 md:mx-10'/></Link>
+            <Link to='/nontechnical'><img src={rightar} className='mx-10 md:mx-10'/></Link>
         </div>
     </div>
     )
 }
 
-export default Creatives;
+export default Nontechnical;
