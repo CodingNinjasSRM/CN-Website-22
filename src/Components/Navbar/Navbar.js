1c63e76275b27cdeefa868de7df7366288716d10
shreenidhi <shreenidhi1757@gmail.com>
2022-12-18 03:41:08 +0530

    events pushed

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -26,7 +26,7 @@ function Navbar() {
         <Link to="/team" className="block py-2 pr-4 pl-3 text-white-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Team</Link>
       </li>
       <li>
-        <a href="#events" className="block py-2 pr-4 pl-3 text-white-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Events</a>
+        <Link to="/events" className="block py-2 pr-4 pl-3 text-white-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Events</Link>
       </li>
     </ul>
   </div>
