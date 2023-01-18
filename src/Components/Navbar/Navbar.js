fa29f562b6aad7ecc5c4159697810c28df97e9da
Paavan Agrawal <33314332+AgrPaavan@users.noreply.github.com>
2023-01-18 18:18:46 +0530

    Remove join us button

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import cnlogo from './assets/cnlogo.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import cnlogo from "./assets/cnlogo.png";
 
 function Navbar() {
   const goToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   };
   return (
@@ -16,23 +16,19 @@ function Navbar() {
           <Link to="/" onClick={goToTop} className="flex items-center mx-2">
             <img src={cnlogo} className="h-6 md:h-8" alt="CN Logo" />
           </Link>
-          <div className="flex md:order-2">
-            <button
-              type="button"
-              className="text-white bg-[#EE4623] font-medium rounded-xl text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-[#EE4623]">
-              Join Us
-            </button>
-          </div>
+          <div className="flex md:order-2 px-5 py-2.5 mr-3 md:mr-0"></div>
           <div
             className="hidden justify-between items-center w-full md:flex ml-10 md:w-auto md:order-1"
-            id="navbar-sticky">
+            id="navbar-sticky"
+          >
             <ul className="flex text-white flex-col p-4 mt-4 border  md:flex-row md:space-x-12 md:mt-0 md:text-sm md:font-medium md:border-0">
               <li>
                 <a
                   href="/#about"
                   onClick={goToTop}
                   className="block py-2 pr-4 pl-3 md:dark:text-white focus:text-white dark:hover:text-white md:text-white-700 md:p-0 "
-                  aria-current="page">
+                  aria-current="page"
+                >
                   About
                 </a>
               </li>
@@ -40,7 +36,8 @@ function Navbar() {
                 <Link
                   to="/domains"
                   onClick={goToTop}
-                  className="block py-2 pr-4 pl-3  md:dark:text-white focus:text-white md:p-0 dark:hover:text-white ">
+                  className="block py-2 pr-4 pl-3  md:dark:text-white focus:text-white md:p-0 dark:hover:text-white "
+                >
                   Domains
                 </Link>
               </li>
@@ -48,7 +45,8 @@ function Navbar() {
                 <Link
                   to="/team"
                   onClick={goToTop}
-                  className="block py-2 pr-4 pl-3  md:dark:text-white focus:text-white  md:p-0 dark:hover:text-white">
+                  className="block py-2 pr-4 pl-3  md:dark:text-white focus:text-white  md:p-0 dark:hover:text-white"
+                >
                   Team
                 </Link>
               </li>
@@ -56,7 +54,8 @@ function Navbar() {
                 <Link
                   to="/events"
                   onClick={goToTop}
-                  className="block py-2 pr-4 pl-3  md:dark:white focus:text-white  md:p-0   dark:hover:text-white ">
+                  className="block py-2 pr-4 pl-3  md:dark:white focus:text-white  md:p-0   dark:hover:text-white "
+                >
                   Events
                 </Link>
               </li>
