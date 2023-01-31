e5957d341ce2888b32b198e7e7677b65e73d0410
AYUSH <ayushrajbls@gmail.com>
2023-01-31 16:53:45 +0530

    Remove Animation

--- src/Components/Technical/Components/AIML.jsx
+++ src/Components/Technical/Components/AIML.jsx
@@ -1,13 +1,12 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import style from "./Domainspage.module.css";
 import leftlogo from "./logo.svg";
 import linelogo from "./logo2.png";
 import Aos from "aos";
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
 import "aos/dist/aos.css";
 
 const AIML = () => {
-
   useEffect(() => {
     Aos.init({ duration: 2000 });
   }, []);
@@ -20,18 +19,18 @@ const AIML = () => {
         <div className="flex flex-col justify-center items-center">
           <p className="text-5xl md:text-7xl font-bold ">TECHNICAL</p>
           <p className="text-3xl md:text-5xl font-medium pb-6">
-          <motion.div
-  animate={{
-    x: 0,
-    y: 0,
-    scale: 1.2,
-    rotate: 0,
-  }}
-  transition={{ duration: 0.6, ease: "easeIn" }}
->
-            AI-ML
-            </motion.div>
-            </p>
+            {/* <motion.div
+              animate={{
+                x: 0,
+                y: 0,
+                scale: 2  ,
+                rotate: 0,
+              }}
+              transition={{ duration: 0.5, ease: "ease-out" }}
+            > */}
+              AI-ML
+            {/* </motion.div> */}
+          </p>
         </div>
       </section>
       <div className="flex flex-col lg:flex-row mt-20 items-center justify-center pb-10 lg:pb-0">
@@ -39,13 +38,15 @@ const AIML = () => {
           <img
             className=" lg:w-[380px] h-auto w-60 md:w-80 mb-10 lg:mb-0 lg:ml-20 lg:mr-20 md "
             src={leftlogo}
-            
             alt=""
           />
         </div>
         <div className=" flex text-center justify-center items-center lg:w-1/2 mx-2 md:mx-10">
           <img className=" mx-2" src={linelogo} alt="" />
-          <p className=" text-white mx-10 text-base md:text-2xl lg:leading-9 lg:text-sm" data-aos="zoom-in">
+          <p
+            className=" text-white mx-10 text-base md:text-2xl lg:leading-9 lg:text-sm"
+            data-aos="zoom-in"
+          >
             The last decade brought AI into the forefront of some of the world's
             most progressive technology companies.With AI and ML we can build
             high performance and data-intensive applications without
