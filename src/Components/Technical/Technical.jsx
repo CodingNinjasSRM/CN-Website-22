535ef1290be8519816b47418da64148bf9ec6636
AYUSH <ayushrajbls@gmail.com>
2023-02-24 20:36:03 +0530

    changed images in technical page

--- src/Components/Technical/Technical.jsx
+++ src/Components/Technical/Technical.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import FooterMin from "../FooterMin/FooterMin";
-import android from "./assets/android.png";
-import c from "./assets/c.png";
-import laptop from "./assets/laptop.png";
+import aimlimg from "./Components/assets/aiml_br.png";
+import webdevimg from "./Components/assets/web_dev_br.png";
+import compimg from "./Components/assets/comp_prog_br.png";
 import ninja from "./assets/ninja.svg";
-import pcb from "./assets/pcb.png";
+import appimg from "./Components/assets/app_dev_br.png";
 import shiruken from "./assets/shiruken.png";
 import styles from "./Technical.module.css";
 
@@ -16,60 +16,84 @@ function Technical() {
     >
       <div className="grid grid-cols-1 grid-rows-4 lg:mt-12  sm:grid-cols-3 sm:grid-rows-2 gap-y-16 sm:gap-y-20 md:gap-20 lg:gap-x-60 lg:gap-y-36">
         <div
-          className={`justify-self-center self-center opacity-0  text-2xl text-white font-semibold ${styles.reveal}`}
+          className={`justify-self-center self-center opacity-0  text-lg text-white font-semibold ${styles.reveal}`}
         >
           <Link to="/technical/cp">
-            <img src={c} alt="" className="hover:animate-bounce" />
-            <p>CP</p>
+            <img
+              src={compimg}
+              alt=""
+              className="transition-transform hover:scale-110 h-56 translate-x-5"
+            />
+            <p className="mt-2 uppercase">competitive programming</p>
           </Link>
         </div>
 
         <div className="row-span-2 justify-self-center self-center hidden sm:flex">
-          <img src={ninja} alt="" className={`h-40 ${styles.flip}`} />
+          <img src={ninja} alt="" className={`h-36 ${styles.flip}`} />
         </div>
 
         <div
-          className={`justify-self-center self-center opacity-0  text-2xl text-white sm:hidden font-semibold ${styles.reveal1}`}
+          className={`justify-self-center self-center opacity-0  text-xl text-white sm:hidden font-semibold ${styles.reveal1}`}
         >
           <Link to="/technical/webdev">
-            <img src={laptop} alt="" className="hover:animate-bounce" />
-            <p>Web Dev</p>
+            <img
+              src={webdevimg}
+              alt=""
+              className="transition-transform hover:scale-110 h-56"
+            />
+             <p className="mt-2">Web Dev</p>
           </Link>
         </div>
 
         <div
-          className={`justify-self-center self-center opacity-0  text-2xl text-white hidden sm:grid  font-semibold ${styles.reveal2}`}
+          className={`justify-self-center self-center opacity-0  text-xl text-white hidden sm:grid  font-semibold ${styles.reveal2}`}
         >
           <Link to="/technical/webdev">
-            <img src={laptop} alt="" className="hover:animate-bounce"/>
-            <p>Web Dev</p>
+            <img
+              src={webdevimg}
+              alt=""
+              className="transition-transform hover:scale-110 h-56"
+            />
+             <p className="mt-2">Web Dev</p>
           </Link>
         </div>
 
         <div
-          className={`justify-self-center self-center opacity-0  text-2xl text-white sm:hidden font-semibold ${styles.reveal2} `}
+          className={`justify-self-center self-center opacity-0  text-xl text-white sm:hidden font-semibold ${styles.reveal2} `}
         >
           <Link to="/technical/aiml">
-            <img src={pcb} alt="" className="hover:animate-bounce"/>
-            <p>AI/ML</p>
+            <img
+              src={aimlimg}
+              alt=""
+              className="transition-transform hover:scale-110 h-56"
+            />
+             <p className="mt-2">AI/ML</p>
           </Link>
         </div>
 
         <div
-          className={`justify-self-center self-center opacity-0  text-2xl text-white hidden sm:grid font-semibold ${styles.reveal1} `}
+          className={`justify-self-center self-center opacity-0  text-xl text-white hidden sm:grid font-semibold ${styles.reveal1} `}
         >
           <Link to="/technical/aiml">
-            <img src={pcb} alt="" className="hover:animate-bounce" />
-            <p>AI/ML</p>
+            <img
+              src={aimlimg}
+              alt=""
+              className="transition-transform hover:scale-110 h-56"
+            />
+            <p className="mt-2">AI/ML</p>
           </Link>
         </div>
 
         <div
-          className={`justify-self-center self-center opacity-0  text-2xl text-white font-semibold ${styles.reveal3}`}
+          className={`justify-self-center self-center opacity-0  text-xl text-white font-semibold ${styles.reveal3}`}
         >
           <Link to="/technical/appdev">
-            <img src={android} alt="" className="hover:animate-bounce" />
-            <p>App Dev</p>
+            <img
+              src={appimg}
+              alt=""
+              className="transition-transform hover:scale-110 h-56"
+            />
+            <p className="mt-2">App Dev</p>
           </Link>
         </div>
       </div>
