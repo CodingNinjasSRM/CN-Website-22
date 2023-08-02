61a675c675b636d777815d5ac1722775eb53e919
AYUSH <ayushrajbls@gmail.com>
2023-08-02 10:37:32 +0530

    Add board data and images

---
+++ src/Components/Team/board.js
@@ -0,0 +1,60 @@
+import cr1 from "./assets/board/MageshCR.jpg"
+import ntl2 from "./assets/board/ManasNonTechLead.png"
+import outreach from "./assets/board/Outreach.png"
+import pres from "./assets/board/President.png"
+import cr2 from "./assets/board/PriyanshiCR.jpg"
+import pl from "./assets/board/ProjectLead.jpg"
+import tl from "./assets/board/TechLead.png"
+import vp from "./assets/board/VicePresident.webp"
+import ntl1 from "./assets/board/WiniNonTechLead.jpg"
+
+export const president = [{
+    name: "Abhishek Dubey",
+    position: "President",
+    image: pres
+}]
+
+export const vicePresident = [{
+    name: "Stuti Gupta",
+    position: "Vice President",
+    image: vp
+}]
+
+export const board = [{
+    name: "Toshak Kaushik",
+    position: "Outreach Officer",
+    image: outreach
+},
+{
+    name: "Magesh Chockalingam",
+    position: "Club Representative",
+    image: cr1
+},
+{
+    name: "Priyanshi Sharma",
+    position: "Club Representative",
+    image: cr2
+}
+]
+
+export const leads = [
+    {
+        name: "Yashaswini Singh Shaktawat",
+        position: "Non-Tech Lead",
+        image: ntl1
+    },
+    {
+        name: "Manas Sharma",
+        position: "Non-Tech Lead",
+        image: ntl2
+    },
+    {
+    name: "Sanskar Gupta",
+    position: "Tech Lead",
+    image: tl
+},
+{
+    name: "Tanishq Kashyap",
+    position: "Project Lead",
+    image: pl
+}]
