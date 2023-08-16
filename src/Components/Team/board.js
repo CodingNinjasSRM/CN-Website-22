30266f4be2a27eddbd509d931b359bbff2d4fa41
AYUSH <ayushrajbls@gmail.com>
2023-08-16 22:01:45 +0530

    update

--- src/Components/Team/board.js
+++ src/Components/Team/board.js
@@ -135,6 +135,12 @@ export const corporate = [
     style: "h-52 w-52",
   },
   {
+    name: "Pradyumn Srivastava",
+    position: "Head",
+    image: corpAssoc3,
+    style: "h-52 w-52",
+  },
+  {
     name: "Daksh",
     position: "Associate",
     image: corpAssoc1,
@@ -146,12 +152,7 @@ export const corporate = [
     image: corpAssoc2,
     style: "h-52 w-52",
   },
-  {
-    name: "Pradyumn Srivastava",
-    position: "Associate",
-    image: corpAssoc3,
-    style: "h-52 w-52",
-  },
+
   {
     name: "Raj",
     position: "Associate",
