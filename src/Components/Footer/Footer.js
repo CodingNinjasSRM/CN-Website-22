a6151abbf01839106aac30d452f0c168fef01e86
Paavan Agrawal <33314332+AgrPaavan@users.noreply.github.com>
2024-07-08 13:56:11 +0530

    Footer fixes

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -41,15 +41,15 @@ function Footer() {
               href="https://medium.com/@cnsrm"
               className="text-orange-700 hover:text-white"
             >
-             <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="md:w-8 md:h-8 w-6 h-6"
-          fill="currentColor"
-          viewBox="0 0 50 50"
-          aria-hidden="true"
-        >
-          <path d="M15 14c6.065 0 11 4.935 11 11s-4.935 11-11 11S4 31.065 4 25 8.935 14 15 14M15 12C7.82 12 2 17.82 2 25s5.82 13 13 13 13-5.82 13-13S22.18 12 15 12L15 12zM35.5 15c2.124 0 4.5 4.277 4.5 10s-2.376 10-4.5 10S31 30.723 31 25 33.376 15 35.5 15M35.5 13c-3.59 0-6.5 5.373-6.5 12s2.91 12 6.5 12S42 31.627 42 25 39.09 13 35.5 13L35.5 13zM46.377 15.667h.005H46.377M45.5 18.577C45.78 20.158 46 22.327 46 25s-.22 4.842-.5 6.423C45.22 29.842 45 27.673 45 25S45.22 20.158 45.5 18.577M45.5 14c-1.381 0-2.5 4.925-2.5 11s1.119 11 2.5 11S48 31.075 48 25 46.881 14 45.5 14L45.5 14z"></path>
-        </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="md:w-8 md:h-8 w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 50 50"
+                aria-hidden="true"
+              >
+                <path d="M15 14c6.065 0 11 4.935 11 11s-4.935 11-11 11S4 31.065 4 25 8.935 14 15 14M15 12C7.82 12 2 17.82 2 25s5.82 13 13 13 13-5.82 13-13S22.18 12 15 12L15 12zM35.5 15c2.124 0 4.5 4.277 4.5 10s-2.376 10-4.5 10S31 30.723 31 25 33.376 15 35.5 15M35.5 13c-3.59 0-6.5 5.373-6.5 12s2.91 12 6.5 12S42 31.627 42 25 39.09 13 35.5 13L35.5 13zM46.377 15.667h.005H46.377M45.5 18.577C45.78 20.158 46 22.327 46 25s-.22 4.842-.5 6.423C45.22 29.842 45 27.673 45 25S45.22 20.158 45.5 18.577M45.5 14c-1.381 0-2.5 4.925-2.5 11s1.119 11 2.5 11S48 31.075 48 25 46.881 14 45.5 14L45.5 14z"></path>
+              </svg>
               <span className="sr-only">Medium page</span>
             </a>
             <a
@@ -135,15 +135,15 @@ function Footer() {
               href="https://medium.com/@cnsrm"
               className="text-orange-700 hover:text-white"
             >
-             <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="md:w-8 md:h-8 w-6 h-6"
-          fill="currentColor"
-          viewBox="0 0 50 50"
-          aria-hidden="true"
-        >
-          <path d="M15 14c6.065 0 11 4.935 11 11s-4.935 11-11 11S4 31.065 4 25 8.935 14 15 14M15 12C7.82 12 2 17.82 2 25s5.82 13 13 13 13-5.82 13-13S22.18 12 15 12L15 12zM35.5 15c2.124 0 4.5 4.277 4.5 10s-2.376 10-4.5 10S31 30.723 31 25 33.376 15 35.5 15M35.5 13c-3.59 0-6.5 5.373-6.5 12s2.91 12 6.5 12S42 31.627 42 25 39.09 13 35.5 13L35.5 13zM46.377 15.667h.005H46.377M45.5 18.577C45.78 20.158 46 22.327 46 25s-.22 4.842-.5 6.423C45.22 29.842 45 27.673 45 25S45.22 20.158 45.5 18.577M45.5 14c-1.381 0-2.5 4.925-2.5 11s1.119 11 2.5 11S48 31.075 48 25 46.881 14 45.5 14L45.5 14z"></path>
-        </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="md:w-8 md:h-8 w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 50 50"
+                aria-hidden="true"
+              >
+                <path d="M15 14c6.065 0 11 4.935 11 11s-4.935 11-11 11S4 31.065 4 25 8.935 14 15 14M15 12C7.82 12 2 17.82 2 25s5.82 13 13 13 13-5.82 13-13S22.18 12 15 12L15 12zM35.5 15c2.124 0 4.5 4.277 4.5 10s-2.376 10-4.5 10S31 30.723 31 25 33.376 15 35.5 15M35.5 13c-3.59 0-6.5 5.373-6.5 12s2.91 12 6.5 12S42 31.627 42 25 39.09 13 35.5 13L35.5 13zM46.377 15.667h.005H46.377M45.5 18.577C45.78 20.158 46 22.327 46 25s-.22 4.842-.5 6.423C45.22 29.842 45 27.673 45 25S45.22 20.158 45.5 18.577M45.5 14c-1.381 0-2.5 4.925-2.5 11s1.119 11 2.5 11S48 31.075 48 25 46.881 14 45.5 14L45.5 14z"></path>
+              </svg>
               <span className="sr-only">Medium Page</span>
             </a>
             <a
@@ -197,12 +197,11 @@ function Footer() {
                   Web Dev
                 </Link>
               </li>
-              <li className="mb-4">
+              {/* <li className="mb-4">
                 <Link to="/technical/webdev" className="">
                   CP
                 </Link>
-              </li>
-
+              </li> */}
             </ul>
             <ul className="text-white dark:text-white">
               <li className="mb-4">
@@ -211,8 +210,8 @@ function Footer() {
                 </Link>
               </li>
               <li className="mb-4">
-                <Link to="/nontechnical/creative" className="">
-                  Creative
+                <Link to="/nontechnical/creatives" className="">
+                  Creatives
                 </Link>
               </li>
               <li className="mb-4">
@@ -234,7 +233,7 @@ function Footer() {
       {/* Copyrights */}
       <div classNameName="dark:bg-neutral-800 dark:bg-opacity-30">
         <div className="mt-7 pt-4 pb-4 text-sm text-white dark:text-stone-200 sm:text-center text-light">
-          © 2023 <a href="/">Coding Ninjas SRM™</a>. All Rights Reserved.
+          © 2024 <a href="/">Coding Ninjas SRM™</a>. All Rights Reserved.
         </div>
       </div>
     </footer>
